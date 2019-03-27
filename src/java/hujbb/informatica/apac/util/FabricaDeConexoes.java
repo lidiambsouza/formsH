7ef70e23b528e158f32d18eda9822c6fbf2f4c95
Leonardo.santos.1 <Leonardo.santos.1@HUJBB-WK-71315.ebserhnet.ebserh.gov.br>
2019-03-27 16:30:02 -0300

    modificacao na view consulta laudo - campo n° do prontuario de volta - troca bd

--- src/java/hujbb/informatica/apac/util/FabricaDeConexoes.java
+++ src/java/hujbb/informatica/apac/util/FabricaDeConexoes.java
@@ -26,15 +26,15 @@ public class FabricaDeConexoes {
 //    private static final String USUARIO = "root";
 //    private static final String SENHA = "";
     
-//    private static final String URL_CONEXAO = "jdbc:mysql://haley.hujbb.br/db_formsus_t";
-//    private static final String USUARIO = "formsus";
-//    private static final String SENHA = "$fs2018&*";
+    private static final String URL_CONEXAO = "jdbc:mysql://haley.hujbb.br/db_formsus_t";
+    private static final String USUARIO = "formsus";
+    private static final String SENHA = "$fs2018&*";
     
     
 
-    private static final String URL_CONEXAO = "jdbc:mysql://10.206.3.110/db_formsus_t";
-    private static final String USUARIO = "gestaoti";
-    private static final String SENHA = "@hu@2018";
+//    private static final String URL_CONEXAO = "jdbc:mysql://10.206.3.110/db_formsus_t";
+//    private static final String USUARIO = "gestaoti";
+//    private static final String SENHA = "@hu@2018";
     
     
 //    private static final String URL_CONEXAO = "jdbc:mysql://10.206.3.110/db_formsus_t3";
